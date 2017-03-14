edc2982a0f8d60c0cf34e9191ee4dc776bccc1b7
telecomdev89 <willsonsami.ws@gmail.com>
2017-03-14 16:22:18 -0400

    Ignored nbproject folder

--- autotester.sh
+++ autotester.sh
@@ -1,4 +1,4 @@
-PORT=5007
+PORT=5000
 FILENAME=Twice.jpg
 
 # BUILD FILES
@@ -19,11 +19,11 @@ sleep .5
 # Starts the clients
 build/Client/client "1" < 'test/c1_input.txt' &
 sleep .01
-build/Client/client "2" < 'test/c2_input.txt' &
-sleep .01
-build/Client/client "3" < 'test/c3_input.txt' &
-sleep .01
-build/Client/client "4" < 'test/c4_input.txt' &
-sleep .01
+#build/Client/client "2" < 'test/c2_input.txt' &
+#sleep .01
+#build/Client/client "3" < 'test/c3_input.txt' &
+#sleep .01
+#build/Client/client "4" < 'test/c4_input.txt' &
+#sleep .01
 
 sleep 10
