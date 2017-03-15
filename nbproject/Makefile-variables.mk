1c7be9df1f03c141817e1d1953c370bb647e3e7d
telecomdev89 <willsonsami.ws@gmail.com>
2017-03-15 06:33:13 -0400

    Added nbproject back

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,59 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Client configuration
+CND_PLATFORM_Client=GNU-Linux
+CND_ARTIFACT_DIR_Client=dist/Client/GNU-Linux
+CND_ARTIFACT_NAME_Client=textconferencinglab
+CND_ARTIFACT_PATH_Client=dist/Client/GNU-Linux/textconferencinglab
+CND_PACKAGE_DIR_Client=dist/Client/GNU-Linux/package
+CND_PACKAGE_NAME_Client=textconferencinglab.tar
+CND_PACKAGE_PATH_Client=dist/Client/GNU-Linux/package/textconferencinglab.tar
+# Server configuration
+CND_PLATFORM_Server=GNU-Linux
+CND_ARTIFACT_DIR_Server=dist/Server/GNU-Linux
+CND_ARTIFACT_NAME_Server=textconferencinglab
+CND_ARTIFACT_PATH_Server=dist/Server/GNU-Linux/textconferencinglab
+CND_PACKAGE_DIR_Server=dist/Server/GNU-Linux/package
+CND_PACKAGE_NAME_Server=textconferencinglab.tar
+CND_PACKAGE_PATH_Server=dist/Server/GNU-Linux/package/textconferencinglab.tar
+# AutoTester configuration
+CND_PLATFORM_AutoTester=GNU-Linux
+CND_ARTIFACT_DIR_AutoTester=dist/AutoTester/GNU-Linux
+CND_ARTIFACT_NAME_AutoTester=textconferencinglab
+CND_ARTIFACT_PATH_AutoTester=dist/AutoTester/GNU-Linux/textconferencinglab
+CND_PACKAGE_DIR_AutoTester=dist/AutoTester/GNU-Linux/package
+CND_PACKAGE_NAME_AutoTester=textconferencinglab.tar
+CND_PACKAGE_PATH_AutoTester=dist/AutoTester/GNU-Linux/package/textconferencinglab.tar
+# Server-Valgrind configuration
+CND_PLATFORM_Server-Valgrind=GNU-Linux
+CND_ARTIFACT_DIR_Server-Valgrind=dist/Server-Valgrind/GNU-Linux
+CND_ARTIFACT_NAME_Server-Valgrind=textconferencinglab
+CND_ARTIFACT_PATH_Server-Valgrind=dist/Server-Valgrind/GNU-Linux/textconferencinglab
+CND_PACKAGE_DIR_Server-Valgrind=dist/Server-Valgrind/GNU-Linux/package
+CND_PACKAGE_NAME_Server-Valgrind=textconferencinglab.tar
+CND_PACKAGE_PATH_Server-Valgrind=dist/Server-Valgrind/GNU-Linux/package/textconferencinglab.tar
+# Client_Valgrind configuration
+CND_PLATFORM_Client_Valgrind=GNU-Linux
+CND_ARTIFACT_DIR_Client_Valgrind=dist/Client_Valgrind/GNU-Linux
+CND_ARTIFACT_NAME_Client_Valgrind=textconferencinglab
+CND_ARTIFACT_PATH_Client_Valgrind=dist/Client_Valgrind/GNU-Linux/textconferencinglab
+CND_PACKAGE_DIR_Client_Valgrind=dist/Client_Valgrind/GNU-Linux/package
+CND_PACKAGE_NAME_Client_Valgrind=textconferencinglab.tar
+CND_PACKAGE_PATH_Client_Valgrind=dist/Client_Valgrind/GNU-Linux/package/textconferencinglab.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ nbproject/private/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+# Client configuration
+# Server configuration
+# AutoTester configuration
+# Server-Valgrind configuration
+# Client_Valgrind configuration
